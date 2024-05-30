d647b74ccea67a7b20c2e552ddedc86a54ad115d
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-05-30 23:20:23 +1000

    added some more unit test

--- script.test.js
+++ script.test.js
@@ -2,6 +2,23 @@ const googleSearch = require("./script");
 
 dbMock = ["god.com", "dog.com", "cheesepuff.com", "dogpictures.com"];
 
-it("this is a test", () => {
-  googleSearch("cat", dbMock);
+describe("googleSearch", () => {
+  it("it is a silly test", () => {
+    // googleSearch("cat", dbMock);
+    expect("hello").toBe("hello");
+  });
+
+  it("is searching google", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+  });
+
+  it("working with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("does not return more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
